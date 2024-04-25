94cbf8e8fae36b4f84f4ae58db67b1c3c4b7cdda
V3NTYY <165188660+V3NTYY@users.noreply.github.com>
2024-04-24 22:27:28 -0500

    Updated Icon

--- src/DM Assistor.cpp
+++ src/DM Assistor.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     // Initialize global strings
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_DMHELPER, szWindowClass, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_DMASSISTOR, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
 
     // Perform application initialization:
@@ -36,7 +36,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         return FALSE;
     }
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DMHELPER));
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DMASSISTOR));
     MSG msg;
 
     // Main message loop:
@@ -64,10 +64,10 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.cbClsExtra     = 0;
     wcex.cbWndExtra     = 0;
     wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DMHELPER));
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DMASSISTOR));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+2);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DMHELPER);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DMASSISTOR);
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
