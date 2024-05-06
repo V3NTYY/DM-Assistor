63acb143ec3e595980a68ab4a9a9efa770a07ec8
V3NTYY <165188660+V3NTYY@users.noreply.github.com>
2024-05-05 19:41:50 -0500

    Create debug tab

--- src/DM Assistor.cpp
+++ src/DM Assistor.cpp
@@ -19,12 +19,13 @@ public:
     virtual bool OnInit();
 };
 
-// MyFrame Class, defines the ribbon and the main window/frame of the applicaton //
+// MyFrame Class, defines the ribbon and the main window/frame of the application //
 class MyFrame : public wxFrame
 {
 public:
     MyFrame(const wxString& title);
-
+    void createCharTab();
+    void createDebugTab();
 private:
     wxNotebook* RIBBON;
 };
@@ -41,11 +42,16 @@ bool MyApp::OnInit()
 wxIMPLEMENT_APP(MyApp);
 
 /// Method that handles creation of the main frame/window.
-MyFrame::MyFrame(const wxString& title)
-    : wxFrame(NULL, wxID_ANY, title)
+MyFrame::MyFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxDefaultSize)
 {
-    // Set the frame/window size, and create "notebook" (a ribbon that holds tabs) at the top.
+    // Set the frame/window size
     this->SetSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	// Prevent resizing of the window
+    this->SetMinSize(wxSize(SCREEN_WIDTH, SCREEN_HEIGHT));
+    this->SetMaxSize(wxSize(SCREEN_WIDTH, SCREEN_HEIGHT));
+
+	// Create the ribbon "notebook" that will hold all tabs
     RIBBON = new wxNotebook(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxNB_TOP);
 
     // Load the icon
@@ -53,18 +59,68 @@ MyFrame::MyFrame(const wxString& title)
     icon.LoadFile("src/resources/DMAssistorMain.ico", wxBITMAP_TYPE_ICO);
     this->SetIcon(icon);
 
-    // Create a sample page
+    // Create all tabs in the ribbon
+    createCharTab();
+    createDebugTab();
+}
+
+// Method that handles the character tab
+void MyFrame::createCharTab()
+{
     wxPanel* CharTab = new wxPanel(RIBBON, wxID_ANY);
     CharTab->SetBackgroundColour(wxColour(111, 55, 205));
-    
-    // Add text
-    wxStaticText* sampleText = new wxStaticText(CharTab, wxID_ANY, "Character Sheet", wxPoint(10, 10), wxSize(100, 100));
-	sampleText->SetFont(wxFont(24, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
 
-    // Create a sample debugging page
-    wxPanel* DebugTab = new wxPanel(RIBBON, wxID_ANY);
-
-	// Add the pages to the ribbon
     RIBBON->AddPage(CharTab, "Character Sheet");
+};
+
+// Method that handles the debug tab
+void MyFrame::createDebugTab()
+{
+    wxPanel* DebugTab = new wxPanel(RIBBON, wxID_ANY);
+    DebugTab->SetBackgroundColour(wxColour(255, 255, 255));
+
+    /// Define all positions and sizes of all elements on the debug tab
+    int inTxtBx[4] = { 10, 10, 200, 100 };
+    int queryBtn[4] = { inTxtBx[0] + inTxtBx[2] + 5, inTxtBx[1], 125, 25 };
+    int encountBx[4] = { queryBtn[0], queryBtn[1] + queryBtn[3] + 5, queryBtn[2], queryBtn[3]};
+    int dialogBx[4] = { queryBtn[0], encountBx[1] + encountBx[3] + 5, queryBtn[2], queryBtn[3]};
+    int outTxtBx[4] = { encountBx[0] + encountBx[2] + 5, inTxtBx[1], SCREEN_WIDTH - (encountBx[0] + encountBx[2]) - 40, inTxtBx[3]};
+
+	/// Create all elements on the debug tab
+    wxTextCtrl* inTxtBox = new wxTextCtrl(DebugTab, wxID_ANY, "", wxPoint(inTxtBx[0], inTxtBx[1]), wxSize(inTxtBx[2], inTxtBx[3]), wxTE_MULTILINE | wxTE_WORDWRAP);
+    wxTextCtrl* outTxtBox = new wxTextCtrl(DebugTab, wxID_ANY, "No response.", wxPoint(outTxtBx[0], outTxtBx[1]), wxSize(outTxtBx[2], outTxtBx[3]), wxTE_MULTILINE | wxTE_WORDWRAP | wxTE_NO_VSCROLL | wxTE_READONLY);
+    wxButton* aiQueryButton = new wxButton(DebugTab, wxID_ANY, "Debug AI Prompt", wxPoint(queryBtn[0], queryBtn[1]), wxSize(queryBtn[2], queryBtn[3]));
+	wxCheckBox* encounterBtn = new wxCheckBox(DebugTab, wxID_ANY, "Generate encounter", wxPoint(encountBx[0], encountBx[1]), wxSize(encountBx[2], encountBx[3]));
+    wxCheckBox* dialogueBtn = new wxCheckBox(DebugTab, wxID_ANY, "Generate dialogue", wxPoint(dialogBx[0], dialogBx[1]), wxSize(dialogBx[2], dialogBx[3]));
+
+    // Event listener for button click
+    aiQueryButton->Bind(wxEVT_BUTTON, [inTxtBox, outTxtBox, encounterBtn, dialogueBtn](wxCommandEvent& event)
+    {
+    	wxString userAIPrompt = inTxtBox->GetValue();
+		if (userAIPrompt.IsEmpty())
+			outTxtBox->SetValue("No response.");
+		else if (encounterBtn->GetValue())
+			outTxtBox->SetValue("ENCOUNTER: " + userAIPrompt);
+		else if (dialogueBtn->GetValue())
+			outTxtBox->SetValue("DIALOGUE: " + userAIPrompt);
+		else
+			outTxtBox->SetValue(userAIPrompt);
+    });
+
+    // Event listeners for checkbox clicks
+    encounterBtn->Bind(wxEVT_CHECKBOX, [encounterBtn, dialogueBtn](wxCommandEvent& event)
+    {
+    	bool isChecked = static_cast<wxCheckBox*>(event.GetEventObject())->GetValue();
+		if (isChecked)
+			dialogueBtn->SetValue(false);
+    });
+
+	dialogueBtn->Bind(wxEVT_CHECKBOX, [encounterBtn, dialogueBtn](wxCommandEvent& event)
+	{
+		bool isChecked = static_cast<wxCheckBox*>(event.GetEventObject())->GetValue();
+        if (isChecked)
+            encounterBtn->SetValue(false);
+	});
+
     RIBBON->AddPage(DebugTab, "Debug Tab");
-}
+};
