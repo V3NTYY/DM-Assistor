c2a00365e1917ea528937e13365393ea6d766434
V3NTYY <165188660+V3NTYY@users.noreply.github.com>
2024-05-25 13:23:58 -0500

    Remove feature for now, base is still present

--- src/headers/Feature.h
+++ src/headers/Feature.h
@@ -9,14 +9,5 @@ public:
         NORMAL
     };
 
-    Feature(FeatureType type, std::function<void(Stat&)> applyFunc)
-        : type(type), applyFunc(std::move(applyFunc)) {}
-
-    void apply(Stat& statBlock) const {
-        applyFunc(statBlock);
-    }
-
 private:
-    FeatureType type;
-    std::function<void(Stat&)> applyFunc;
 };
